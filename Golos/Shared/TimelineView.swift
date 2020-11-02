6abeb5308470f1c9916ea2dbcd1cc0ba6134c3f0
Felix Green <work@felix.green>
2020-11-01 22:43:39 -0500

    add placeholder view for timeline

---
+++ Golos/Shared/TimelineView.swift
@@ -0,0 +1,24 @@
+//
+//  TimelineView.swift
+//  Golos
+//
+//  Created by Felix Akira Green on 11/1/20.
+//
+
+import SwiftUI
+
+struct TimelineView: View {
+	var body: some View {
+		HStack {
+			Spacer()
+			Text("Timeline")
+			Spacer()
+		}
+	}
+}
+
+struct TimelineView_Previews: PreviewProvider {
+	static var previews: some View {
+		TimelineView()
+	}
+}
