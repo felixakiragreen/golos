7bba5fc08c4c17f3b8863e588ccb1d92ded2e216
Felix Green <work@felix.green>
2020-11-07 15:07:06 -0500

    cleanup experiments, add NowView

--- Golos/Shared/OverView.swift
+++ Golos/Shared/OverView.swift
@@ -8,57 +8,27 @@
 import SwiftUI
 
 struct OverView: View {
-	@State var percentValue: Double = 20
-	
 	var body: some View {
 		VStack {
 			VStack {
 				Spacer()
 				HStack {
 					Text("Past")
-					Text("Present")
+					NowView()
 					Text("Future")
-					
-					SquareBrick(percent: percentValue)
-						.frame(width: 20, height: 20)
 				}
+			
 				Spacer()
-//				.frame(
-//					maxWidth: .infinity,
-//					maxHeight: .infinity
-//				)
-				
-				Slider(value: $percentValue, in: 0...100)
-				
-				Button("Random Progress", action: {
-					withAnimation {
-						let newPercent = Double.random(in: 0...100)
-						percentValue = newPercent
-					}
-				})
-//					.onTapGesture {
-//					let newPercent = CGFloat.random(in: 0...100)
-//					print(newPercent)
-//					self.percentValue = newPercent
-//				}
-				
 				TimelineView()
 				Spacer()
+				
 				HStack {
 					Text("Objectives")
 					Text("Chracter")
 					Text("Metrics")
 				}
 				Spacer()
-//				.frame(
-//					maxWidth: .infinity,
-//					maxHeight: .infinity
-//				)
 			}
-			.frame(
-				maxWidth: .infinity,
-				maxHeight: .infinity
-			)
 		}
 		.navigationTitle("Overview")
 		.frame(
@@ -68,8 +38,26 @@ struct OverView: View {
 			minHeight: 480,
 			idealHeight: 800,
 			maxHeight: .infinity
-//			alignment: .leading
 		)
+	}
+}
+
+struct AnimationExperiment: View {
+	@State var percentValue: Double = 20
+	
+	var body: some View {
+		HStack {
+			SquareBrick(percent: percentValue)
+				.frame(width: 20, height: 20)
+			Slider(value: $percentValue, in: 0...100)
+			
+			Button("Random Progress", action: {
+				withAnimation {
+					let newPercent = Double.random(in: 0...100)
+					percentValue = newPercent
+				}
+			})
+		}
 	}
 }
 
