4a4bb0102402d657e76f2bc50ab7aea7b0d09009
Felix Green <work@felix.green>
2020-11-07 17:57:53 -0500

    add comments & unused StringInterpolation method

--- Golos/Shared/Extensions.swift
+++ Golos/Shared/Extensions.swift
@@ -3,22 +3,39 @@
 //  Golos
 //
 //  Created by Felix Akira Green on 11/6/20.
-//  Copied from https://www.raywenderlich.com/7589178-how-to-create-a-neumorphic-design-with-swiftui#toc-anchor-012
+//
 
 import SwiftUI
 
+// Copied from https://www.raywenderlich.com/7589178-how-to-create-a-neumorphic-design-with-swiftui#toc-anchor-012
 extension View {
-	// 1
+	// Define a new inverseMask that mimics mask
 	func inverseMask<Mask>(_ mask: Mask) -> some View where Mask: View {
-		// 2
+		// Return the current view masked with the input mask and modified.
 		self.mask(mask
-			// 3
+			// Set the foreground color of the input mask to black.
 			.foregroundColor(.black)
-			// 4
+			// Ensure the background of the input mask is solid white.
 			.background(Color.white)
-			// 5
+			// Wrap the input mask in a compositing group.
 			.compositingGroup()
-			// 6
+			// Converted the luminance to alpha, turning the black foreground transparent and keeping the light background opaque — i.e., an inverse mask!
 			.luminanceToAlpha())
 	}
 }
+
+// Copied from https://www.hackingwithswift.com/articles/178/super-powered-string-interpolation-in-swift-5-0
+// But no longer using. May remove later but it's a handy reference.
+/*
+extension String.StringInterpolation {
+	mutating func appendInterpolation(noSeparator value: Int) {
+		let formatter = NumberFormatter()
+		formatter.numberStyle = .decimal
+		formatter.groupingSeparator = ""
+
+		if let result = formatter.string(from: value as NSNumber) {
+			appendLiteral(result)
+		}
+	}
+}
+*/
