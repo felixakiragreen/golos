396826d1ca69f6097702b1f1996afeaedb99afbb
Felix Green <work@felix.green>
2020-11-07 14:08:16 -0500

    code formatting

--- Golos/Shared/Extensions.swift
+++ Golos/Shared/Extensions.swift
@@ -8,18 +8,17 @@
 import SwiftUI
 
 extension View {
-  // 1
-  func inverseMask<Mask>(_ mask: Mask) -> some View where Mask: View {
-	 // 2
-	 self.mask(mask
-		// 3
-		.foregroundColor(.black)
-		// 4
-		.background(Color.white)
-		// 5
-		.compositingGroup()
-		// 6
-		.luminanceToAlpha()
-	 )
-  }
+	// 1
+	func inverseMask<Mask>(_ mask: Mask) -> some View where Mask: View {
+		// 2
+		self.mask(mask
+			// 3
+			.foregroundColor(.black)
+			// 4
+			.background(Color.white)
+			// 5
+			.compositingGroup()
+			// 6
+			.luminanceToAlpha())
+	}
 }
