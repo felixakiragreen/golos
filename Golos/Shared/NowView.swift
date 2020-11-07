47e7fca09582b4d988a78d00c19cd911380a2bfe
Felix Green <work@felix.green>
2020-11-07 15:06:49 -0500

    stub out a NowView for doing all the things today

---
+++ Golos/Shared/NowView.swift
@@ -0,0 +1,20 @@
+//
+//  NowView.swift
+//  Golos
+//
+//  Created by Felix Akira Green on 11/7/20.
+//
+
+import SwiftUI
+
+struct NowView: View {
+	var body: some View {
+		Text("Present")
+	}
+}
+
+struct NowView_Previews: PreviewProvider {
+	static var previews: some View {
+		NowView()
+	}
+}
