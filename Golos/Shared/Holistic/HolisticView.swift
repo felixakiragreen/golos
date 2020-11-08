089b6eb06ab55e5a75fa235f79ee7d315bc163c2
Felix Green <work@felix.green>
2020-11-07 21:03:01 -0500

    stub HolisticView

---
+++ Golos/Shared/Holistic/HolisticView.swift
@@ -0,0 +1,24 @@
+//
+//  HolisticView.swift
+//  Golos
+//
+//  Created by Felix Akira Green on 11/7/20.
+//
+
+import SwiftUI
+
+struct HolisticView: View {
+	var body: some View {
+		HStack {
+			Text("Objectives")
+			Text("Chracter")
+			Text("Metrics")
+		}
+	}
+}
+
+struct HolisticView_Previews: PreviewProvider {
+	static var previews: some View {
+		HolisticView()
+	}
+}
