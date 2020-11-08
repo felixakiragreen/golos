2a3e62e6982badcc337236f0dd3a215456d2471d
Felix Green <work@felix.green>
2020-11-07 21:02:39 -0500

    stub PlannerView

---
+++ Golos/Shared/Planner/PlannerView.swift
@@ -0,0 +1,22 @@
+//
+//  PlannerView.swift
+//  Golos
+//
+//  Created by Felix Akira Green on 11/7/20.
+//
+
+import SwiftUI
+
+struct PlannerView: View {
+	var body: some View {
+		HStack {
+			NowView()
+		}
+	}
+}
+
+struct PlannerView_Previews: PreviewProvider {
+	static var previews: some View {
+		PlannerView()
+	}
+}
