625cdb4f6ea2d5a3fa23d7a3a2566736c2ecf09e
Felix Green <work@felix.green>
2020-11-14 21:49:47 -0500

    Create SetupView.swift

---
+++ Golos/Shared/SetupView.swift
@@ -0,0 +1,20 @@
+//
+//  SetupView.swift
+//  Golos
+//
+//  Created by Felix Akira Green on 11/14/20.
+//
+
+import SwiftUI
+
+struct SetupView: View {
+    var body: some View {
+        Text("We will do all the setup here.")
+    }
+}
+
+struct SetupView_Previews: PreviewProvider {
+    static var previews: some View {
+        SetupView()
+    }
+}
