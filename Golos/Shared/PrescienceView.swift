54494d1bff516e1ff7dde6793d168b0ee2b0dc05
Felix Green <work@felix.green>
2020-11-21 22:12:44 -0500

    add prescience stub

---
+++ Golos/Shared/PrescienceView.swift
@@ -0,0 +1,26 @@
+//
+//  PrescienceView.swift
+//  Golos
+//
+//  Created by Felix Akira Green on 11/21/20.
+//
+
+import SwiftUI
+
+
+/*
+
+
+
+*/
+struct PrescienceView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct PrescienceView_Previews: PreviewProvider {
+    static var previews: some View {
+        PrescienceView()
+    }
+}
