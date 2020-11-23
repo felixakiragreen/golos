f6d08aa46d26a273c39c3df80549d1284e16959d
Felix Green <work@felix.green>
2020-11-22 23:29:00 -0500

    stub out the rest of the Prescience Module

---
+++ Golos/Shared/Prescience/PrescienceModel.swift
@@ -0,0 +1,20 @@
+//
+//  PrescienceModel.swift
+//  Golos
+//
+//  Created by Felix Akira Green on 11/22/20.
+//
+
+import SwiftUI
+
+struct PrescienceModel: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct PrescienceModel_Previews: PreviewProvider {
+    static var previews: some View {
+        PrescienceModel()
+    }
+}
---
+++ Golos/Shared/Prescience/PrescienceSetupView.swift
@@ -0,0 +1,20 @@
+//
+//  PrescienceSetupView.swift
+//  Golos
+//
+//  Created by Felix Akira Green on 11/22/20.
+//
+
+import SwiftUI
+
+struct PrescienceSetupView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct PrescienceSetupView_Previews: PreviewProvider {
+    static var previews: some View {
+        PrescienceSetupView()
+    }
+}
