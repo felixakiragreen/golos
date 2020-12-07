1a69723a40dfe36408df9892e24274716420f6ef
Felix Green <work@felix.green>
2020-12-07 13:30:25 -0500

    new Xcode Project for layout exploration

---
+++ golOS layout/Shared/golOS_layoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  golOS_layoutApp.swift
+//  Shared
+//
+//  Created by Felix Akira Green on 12/7/20.
+//
+
+import SwiftUI
+
+@main
+struct golOS_layoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
