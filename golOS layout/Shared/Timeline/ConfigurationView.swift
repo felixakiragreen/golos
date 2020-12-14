b8a985b00f7fc8007fb7970337991cdf4d44403e
Felix Green <work@felix.green>
2020-12-13 19:15:18 -0500

    add a view for the configuring the Timeline

---
+++ golOS layout/Shared/Timeline/ConfigurationView.swift
@@ -0,0 +1,20 @@
+//
+//  ConfigurationView.swift
+//  golOS layout
+//
+//  Created by Felix Akira Green on 12/13/20.
+//
+
+import SwiftUI
+
+struct TimelineConfigurationView: View {
+	var body: some View {
+		Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+	}
+}
+
+struct TimelineConfigurationView_Previews: PreviewProvider {
+	static var previews: some View {
+		TimelineConfigurationView()
+	}
+}
