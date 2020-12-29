d6e3d840b6f3ff620f881d4179907cc0d27a5a7b
Felix Green <work@felix.green>
2020-12-29 18:25:14 -0500

    stub the Detail view

---
+++ golOS/Shared/DetailView.swift
@@ -0,0 +1,20 @@
+//
+//  DetailView.swift
+//  golOS
+//
+//  Created by Felix Akira Green on 12/29/20.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+	var body: some View {
+		Text("Hello, World!")
+	}
+}
+
+struct DetailView_Previews: PreviewProvider {
+	static var previews: some View {
+		DetailView()
+	}
+}
