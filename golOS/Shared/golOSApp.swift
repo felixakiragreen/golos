3c42c8bb7521728eaa89ecaefacb7389b64836e2
Felix Green <work@felix.green>
2020-12-28 21:48:47 -0500

    create another project

---
+++ golOS/Shared/golOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  golOSApp.swift
+//  Shared
+//
+//  Created by Felix Akira Green on 12/28/20.
+//
+
+import SwiftUI
+
+@main
+struct golOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
