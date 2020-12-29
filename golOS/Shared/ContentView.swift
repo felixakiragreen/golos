07e4172874309344991d843dd4eee928f15e7cb0
Felix Green <work@felix.green>
2020-12-29 18:25:40 -0500

    completely restyle the Group & Buttons

--- golOS/Shared/ContentView.swift
+++ golOS/Shared/ContentView.swift
@@ -7,31 +7,66 @@
 
 import SwiftUI
 
+// MARK: - PREVIEW
+struct ContentView_Previews: PreviewProvider {
+	static var previews: some View {
+		NavigationView {
+			ContentView()
+		}
+//		.preferredColorScheme(.dark)
+	}
+}
+
+
 struct ContentView: View {
+	// MARK: - PROPS
 	@State private var isPresented = false
 
+	// MARK: - BODY
 	var body: some View {
 		ZStack {
 			Color("grey.sys.100").edgesIgnoringSafeArea(.all)
 			VStack {
 				HStack {
-					Text("Hello, world!")
-						.padding()
+//					Text("Hello, world!")
+//						.padding()
 				}
 				Spacer()
 				VStack(spacing: 16) {
-					HStack(alignment: .firstTextBaseline) {
-						Spacer()
-						Text("critical")
-							.foregroundColor(Color("red.sys.900"))
-							.font(Font.system(.title2, design: .rounded))
-							.bold()
-						Text("survive")
-							.foregroundColor(Color("red.sys.700"))
-						Spacer()
+					GroupAnnotationDimension(colorHue: .red) {
+						VStack {
+							HStack {
+								ButtonAnnotationType(
+									colorHue: .purple,
+									title: "sleep",
+									icon: "bed.double.fill",
+									trailing: true) {
+									isPresented = true
+								}
+								ButtonAnnotationType(
+									colorHue: .blue,
+									title: "social",
+									icon: "person.3.fill") {
+									isPresented = true
+								}
+							}
+							HStack {
+								ButtonAnnotationType(
+									colorHue: .red,
+									title: "fitness",
+									icon: "figure.walk",
+									trailing: true) {
+									isPresented = true
+								}
+								ButtonAnnotationType(
+									colorHue: .green,
+									title: "diet",
+									icon: "mouth.fill") {
+									isPresented = true
+								}
+							}
+						}
 					}
-					.frame(minHeight: 100)
-					.background(Color("red.sys.300"))
 
 					HStack(alignment: .firstTextBaseline) {
 						Spacer()
@@ -61,29 +96,138 @@ struct ContentView: View {
 				}
 			}
 		}
-		.navigationTitle("Daily")
-//		.sheet(isPresented: $isPresented) {
-//			NavigationView {
-//				EditView(scrumData: $newScrumData)
-//					.navigationBarItems(leading: Button("Dismiss") {
-//						isPresented = false
-//					}, trailing: Button("Add") {
+//		.navigationTitle("Daily")
+		.sheet(isPresented: $isPresented) {
+			NavigationView {
+				DetailView()
+					.navigationBarItems(leading: Button("Dismiss") {
+						isPresented = false
+					}
+//					, trailing: Button("Add") {
 //						let newScrum = DailyScrum(title: newScrumData.title, attendees: newScrumData.attendees,
-//						                          lengthInMinutes: Int(newScrumData.lengthInMinutes), color: newScrumData.color)
+//														  lengthInMinutes: Int(newScrumData.lengthInMinutes), color: newScrumData.color)
 //						scrums.append(newScrum)
 //						isPresented = false
-//					})
-//			}
-//		}
+//					}
+					)
+//				EditView(scrumData: $newScrumData)
+			}
+		}
 //		.frame(minWidth: .infinity, minHeight: .infinity)
 	}
 }
 
-struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		NavigationView {
-			ContentView()
+
+// MARK: - SUBVIEWS
+
+struct GroupAnnotationDimension<Content: View>: View {
+	var colorHue: ColorHue
+	var content: () -> Content
+	
+	var body: some View {
+		VStack(spacing: 8) {
+			
+			HStack(alignment: .firstTextBaseline) {
+				Spacer()
+				Text("critical")
+					.foregroundColor(ColorPreset(hue: colorHue, lum: .nearBlack).getColor())
+					.font(Font.system(.title2, design: .rounded))
+					.bold()
+				Text("survive")
+					.foregroundColor(ColorPreset(hue: colorHue, lum: .dark).getColor())
+				Spacer()
+			}//: LABEL
+			.padding(.horizontal, 12)
+			
+			HStack {
+				content()
+			}//: CONTENT
+			.padding()
+			.frame(maxWidth: .infinity)
+			.background(
+				ZStack {
+					ColorPreset(hue: colorHue, lum: .extraLight).getColor()
+					
+					FlatHexagonalShape(body: .infinity)
+						.inset(by: -6)
+						.fill(ColorPreset(hue: .grey, lum: .nearWhite).getColor())
+					
+					FlatHexagonalShape(body: .infinity)
+						.inset(by: 3)
+						.fill(ColorPreset(hue: .grey, lum: .extraLight).getColor())
+
+					FlatHexagonalShape(body: .infinity)
+						.strokeBorder(
+							LinearGradient(gradient: Gradient.init(colors: [
+								ColorPreset(hue: .red, lum: .normal).getColor(),
+								ColorPreset(hue: .red, lum: .nearWhite).getColor(),
+								ColorPreset(hue: .red, lum: .normal).getColor()
+							]),
+							startPoint: .leading, endPoint: .trailing
+							),
+							lineWidth: 1
+						)
+				}
+			)
 		}
-		.preferredColorScheme(.dark)
+		
+	}
+}
+
+struct ButtonAnnotationType: View {
+	var colorHue: ColorHue
+	var title: String
+	var icon: String
+	var trailing: Bool = false
+	var action: () -> Void
+	
+	var body: some View {
+		Button(action: action) {
+			HStack(spacing: 12) {
+				if trailing {
+					bodyText
+					bodyIcon
+				} else {
+					bodyIcon
+					bodyText
+				}
+			}
+			.foregroundColor(ColorPreset(hue: colorHue, lum: .extraDark).getColor())
+			.padding(6)
+			.padding(.horizontal, 12)
+//			.padding(.leading, trailing ? 18 : 12)
+//			.padding(.trailing, trailing ? 12 : 18)
+			.background(
+				FlatHexagon(
+					fill: ColorPreset(hue: colorHue, lum: .extraLight).getColor(),
+					length: .infinity
+				)
+			)
+		}
+	}
+	
+	var bodyIcon: some View {
+		ZStack {
+			PointyHexagonalShape()
+				.fill(ColorPreset(hue: colorHue, lum: .dark).getColor())
+				.hexagonalFrame(height: 12)
+				.overlay(
+					PointyHexagonalShape()
+						.inset(by: -6)
+						.strokeBorder(ColorPreset(hue: .red, lum: .medium).getColor(), lineWidth: 3)
+				)
+//			Image(systemName: "\(icon)")
+		}.padding(.horizontal, 6)
+	}
+	
+	var bodyText: some View {
+		HStack(spacing: 0) {
+			Text("~")
+				.font(Font.system(.body, design: .monospaced))
+				.foregroundColor(ColorPreset(hue: .red, lum: .dark).getColor())
+			Text(title)
+				.font(Font.system(.headline, design: .monospaced))
+			
+		}
 	}
 }
