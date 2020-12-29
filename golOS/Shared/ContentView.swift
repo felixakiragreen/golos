9384024a3be6587ceba4c4139395dc4716b4b66f
Felix Green <work@felix.green>
2020-12-28 22:20:29 -0500

    stub out a bit of stuffs

--- golOS/Shared/ContentView.swift
+++ golOS/Shared/ContentView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ContentView: View {
+	@State private var isPresented = false
+
 	var body: some View {
 		ZStack {
 			Color("grey.sys.100").edgesIgnoringSafeArea(.all)
@@ -15,29 +17,43 @@ struct ContentView: View {
 				HStack {
 					Text("Hello, world!")
 						.padding()
-					
 				}
 				Spacer()
 				VStack(spacing: 16) {
-					HStack {
+					HStack(alignment: .firstTextBaseline) {
 						Spacer()
+						Text("critical")
+							.foregroundColor(Color("red.sys.900"))
+							.font(Font.system(.title2, design: .rounded))
+							.bold()
 						Text("survive")
+							.foregroundColor(Color("red.sys.700"))
 						Spacer()
 					}
 					.frame(minHeight: 100)
 					.background(Color("red.sys.300"))
-					
-					HStack {
+
+					HStack(alignment: .firstTextBaseline) {
 						Spacer()
+						Text("productive")
+							.foregroundColor(Color("green.sys.900"))
+							.font(Font.system(.title2, design: .rounded))
+							.bold()
 						Text("strive")
+							.foregroundColor(Color("green.sys.700"))
 						Spacer()
 					}
 					.frame(minHeight: 100)
 					.background(Color("green.sys.300"))
-					
-					HStack {
+
+					HStack(alignment: .firstTextBaseline) {
 						Spacer()
+						Text("non-productive")
+							.foregroundColor(Color("blue.sys.900"))
+							.font(Font.system(.title2, design: .rounded))
+							.bold()
 						Text("thrive")
+							.foregroundColor(Color("blue.sys.700"))
 						Spacer()
 					}
 					.frame(minHeight: 100)
@@ -45,6 +61,20 @@ struct ContentView: View {
 				}
 			}
 		}
+		.navigationTitle("Daily")
+//		.sheet(isPresented: $isPresented) {
+//			NavigationView {
+//				EditView(scrumData: $newScrumData)
+//					.navigationBarItems(leading: Button("Dismiss") {
+//						isPresented = false
+//					}, trailing: Button("Add") {
+//						let newScrum = DailyScrum(title: newScrumData.title, attendees: newScrumData.attendees,
+//						                          lengthInMinutes: Int(newScrumData.lengthInMinutes), color: newScrumData.color)
+//						scrums.append(newScrum)
+//						isPresented = false
+//					})
+//			}
+//		}
 //		.frame(minWidth: .infinity, minHeight: .infinity)
 	}
 }
