1b42964b1b4101843f8d7c898ea8aa0518cd606d
Felix Green <work@felix.green>
2020-12-28 22:06:16 -0500

    stub out some areas

--- golOS/Shared/ContentView.swift
+++ golOS/Shared/ContentView.swift
@@ -9,13 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
 	var body: some View {
-		Text("Hello, world!")
-			.padding()
+		ZStack {
+			Color("grey.sys.100").edgesIgnoringSafeArea(.all)
+			VStack {
+				HStack {
+					Text("Hello, world!")
+						.padding()
+					
+				}
+				Spacer()
+				VStack(spacing: 16) {
+					HStack {
+						Spacer()
+						Text("survive")
+						Spacer()
+					}
+					.frame(minHeight: 100)
+					.background(Color("red.sys.300"))
+					
+					HStack {
+						Spacer()
+						Text("strive")
+						Spacer()
+					}
+					.frame(minHeight: 100)
+					.background(Color("green.sys.300"))
+					
+					HStack {
+						Spacer()
+						Text("thrive")
+						Spacer()
+					}
+					.frame(minHeight: 100)
+					.background(Color("blue.sys.300"))
+				}
+			}
+		}
+//		.frame(minWidth: .infinity, minHeight: .infinity)
 	}
 }
 
 struct ContentView_Previews: PreviewProvider {
 	static var previews: some View {
-		ContentView()
+		NavigationView {
+			ContentView()
+		}
+		.preferredColorScheme(.dark)
 	}
 }
