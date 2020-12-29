52e18437ad13d734aaef3c711d8336fe10731c53
Felix Green <work@felix.green>
2020-12-28 21:49:06 -0500

    format

--- golOS/Shared/golOSApp.swift
+++ golOS/Shared/golOSApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct golOSApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
-    }
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
 }
