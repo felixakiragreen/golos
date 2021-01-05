c78420b92e5aab61825651a43ba8324a896053b1
Felix Green <work@felix.green>
2021-01-04 23:38:14 -0500

    load data from json

--- golOS/Shared/golOSApp.swift
+++ golOS/Shared/golOSApp.swift
@@ -9,10 +9,19 @@ import SwiftUI
 
 @main
 struct golOSApp: App {
+	@ObservedObject private var data = TemporalData()
+
 	var body: some Scene {
 		WindowGroup {
 			NavigationView {
-				ContentView()
+//				ContentView()
+//				TemporalEntry()
+				DailyFlow(dailyAnnotations: $data.daily) {
+					data.save()
+				}
+			}
+			.onAppear {
+				data.load()
 			}
 		}
 	}
