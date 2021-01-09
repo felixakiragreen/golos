9c7da5811d1877eee977c6480b0ae2ed6b07ff2c
Felix Green <work@felix.green>
2021-01-08 21:27:05 -0500

    add some helpful calendar enumeration stuff

---
+++ golOS/Shared/Model/Calendar+generate.swift
@@ -0,0 +1,34 @@
+//
+//  Calendar+generate.swift
+//  golOS
+//
+//  Created by Felix Akira Green on 1/8/21.
+//
+
+import Foundation
+
+extension Calendar {
+	func generate(
+		inside interval: DateInterval,
+		matching components: DateComponents
+	) -> [Date] {
+		var dates: [Date] = []
+//		dates.append(interval.start)
+
+		enumerateDates(
+			startingAfter: interval.start,
+			matching: components,
+			matchingPolicy: .nextTime
+		) { date, _, stop in
+			if let date = date {
+				if date < interval.end {
+					dates.append(date)
+				} else {
+					stop = true
+				}
+			}
+		}
+
+		return dates
+	}
+}
