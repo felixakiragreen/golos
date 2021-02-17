e92fdfa9d2db447d6fa7dacdf3953b2a3773ab7b
Felix Green <work@felix.green>
2021-02-17 15:59:17 -0500

    stub out AppModel

---
+++ golOS/Shared/Model/AppModel.swift
@@ -0,0 +1,21 @@
+//
+//  AppModel.swift
+//  golOS
+//
+//  Created by Felix Akira Green on 2/17/21.
+//
+
+import SwiftUI
+
+class AppModel: ObservableObject {
+	// @Environment(\.calendar) var calendar: Calendar
+	// @Environment(\.physicalSpec) var location: PhysicalSpec
+	
+	
+	// @ObservedObject var solarModel: SolarModel
+	
+	init() {
+		
+	}
+	
+}
